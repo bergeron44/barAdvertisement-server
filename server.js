e136bb1415e85433bab3cf13782392cb3228144c
ron berger <ronberger40@gmail.com>
2024-11-12 21:05:25 +0200

    Initial commit for the backend

--- server.js
+++ server.js
@@ -57,6 +57,89 @@ let bars = [
         discountOne: "🍸 משחק דייטים : 2 קוקטיילים ב-80 ש״ח בלבד",
         discountSec: "🍻 משחק חברים : 3 בירות ו-5 שוטים ב-150 ש״ח",
         discountThi: "🍷 משחק צחוקים : כל כוס חמישי חינם 🍷"
+    },
+    {
+        id: 5,
+        name: "Roots",
+        lat: 31.2679,
+        lng: 34.8000,
+        instagram: "https://www.instagram.com/rootsbarkitchen/",
+        videoUrl: "https://example.com/video11.mp4",
+        likes: 0,
+        imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSP3-R6lwS-UvfwtCtPgS2cTIvDX_yIYCuqlg&s",
+        discountOne: "🍸 משחק דייטים : 2 קוקטיילים ב-70 ש״ח בלבד",
+        discountSec: "🍻 משחק חברים : 3 בירות ב-90 ש״ח",
+        discountThi: "🥂 משחק צחוקים : כל שוט שלישי בחינם"
+    },
+    {
+        id: 6,
+        name: "Masada31",
+        lat: 31.2589,
+        lng: 34.7935,
+        website: "https://www.facebook.com/p/%D7%9E%D7%A6%D7%93%D7%94-31-100084933198419/",
+        instagram: "",  // אם יש חשבון אינסטגרם ניתן להוסיף כאן
+        videoUrl: "https://example.com/video13.mp4",  // ניתן להוסיף קישור לסרטון אם יש
+        likes: 0,
+        imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFpAH51toe9nLLrlGejnUzJgyr3dnxFtc8dw&s",  // ניתן להוסיף תמונת ברירת מחדל או כתובת לתמונה מתאימה
+        discountOne: "🍸 מבצע 1: 2 קוקטיילים ב-70 ש״ח בלבד",
+        discountSec: "🍻 מבצע 2: 3 בירות ו-4 שוטים ב-130 ש״ח",
+        discountThi: "🥂 מבצע 3: כל כוס חמישי בחינם"
+    },
+    {
+        id: 7,
+        name: "Friends",
+        lat: 31.2584, // עדכן עם קואורדינטות מדויקות אם נדרש
+        lng: 34.7952, // עדכן עם קואורדינטות מדויקות אם נדרש
+        website: "https://www.facebook.com/friends1malabi/", // קישור לפייסבוק של הבר
+        instagram: "",  // אם יש אינסטגרם, ניתן להוסיף קישור כאן
+        videoUrl: "",  // אם יש סרטון, ניתן להוסיף קישור כאן
+        likes: 0,
+        imageUrl: "https://example.com/image15.jpg",  // תמונה מתאימה לבר
+        discountOne: "🍨 משחקי השתייה: כל שוט חמישי חינם",
+        discountSec: "🍻 1+1 על בירות בימי ראשון",
+        discountThi: "🍹 2 קוקטיילים במחיר אחד אחרי 10 בלילה"
+    },
+    {
+        id: 8,
+        name: "BarBaSaba",
+        lat: 31.2476, 
+        lng: 34.7988, 
+        website: "https://www.facebook.com/BarBaSaba",  // קישור חדש לפייסבוק
+        instagram: "https://www.instagram.com/explore/locations/238247954/barbasaba-beer-garden/",  // קישור לאינסטגרם
+        videoUrl: "",  // הוסף סרטון אם יש
+        likes: 0,
+        imageUrl: "https://example.com/imageBarBaSaba.jpg",  // הוסף תמונה מתאימה
+        discountOne: "🍸 הנחה על קוקטיילים",
+        discountSec: "🍻 הנחה על בירות",
+        discountThi: "🍷 הנחה על משקאות נוספים"
+    },
+    {
+        id: 9,
+        name: "JEMS",
+        lat: 31.2634, 
+        lng: 34.8106, 
+        website: "https://www.jems.co.il/branches/beer-sheva/",  // קישור לאתר הרשמי
+        facebook: "https://www.facebook.com/p/Jems-Beer-Sheva-%D7%92%D7%9E%D7%A1-%D7%91%D7%90%D7%A8-%D7%A9%D7%91%D7%A2-100063714934152/?_rdr",  // קישור לפייסבוק
+        videoUrl: "",  // הוסף סרטון אם יש
+        likes: 0,
+        imageUrl: "https://example.com/imageJems.jpg",  // הוסף תמונה מתאימה
+        discountOne: "🍸 הנחה על קוקטיילים",
+        discountSec: "🍻 הנחה על בירות",
+        discountThi: "🍷 הנחה על משקאות נוספים"
+    },
+    {
+        id: 10,  // Update the ID as needed
+        name: "Giza",
+        lat: 31.2547, 
+        lng: 34.7909, 
+        website: "https://www.gizabar.com/",  // Link to official website
+        facebook: "https://www.instagram.com/giza.bar/",  // Link to Instagram (Facebook if needed)
+        videoUrl: "",  // Add video URL if available
+        likes: 0,
+        imageUrl: "https://example.com/imageGiza.jpg",  // Replace with an actual image URL
+        discountOne: "🍸 הנחה על קוקטיילים",
+        discountSec: "🍻 הנחה על בירות",
+        discountThi: "🍷 הנחה על משקאות נוספים"
     }
 ];
 
